50d081e27eafd61f63369ef4732b13a45e322b5b
Zenox <108974651+Giangminhtien@users.noreply.github.com>
2023-09-23 20:12:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+x = 0.0;
+y = 0.0;
+option = 0
+
+function calculate() {
+  x = document.getElementById("x").value;
+  y = document.getElementById("y").value;
+  option = document.getElementById("option").value;
+  x = Number(x);
+  y = Number(y);
+  option = Number(option);
+  if (option === 1) {
+    alert(x + y);
+  } else if (option === 2) {
+    alert(x - y);
+  } else if (option === 3) {
+    alert(x * y);
+  } else {
+    alert(x / y);
+  }
+}
